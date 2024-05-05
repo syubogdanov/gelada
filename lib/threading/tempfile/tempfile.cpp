2c10f295ae616ae2af7a9770a6f2399bfa5c6faa
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-05-06 00:58:55 +0300

    DEV - implement thread-safe creation of temporary files

---
+++ lib/threading/tempfile/tempfile.cpp
@@ -0,0 +1,46 @@
+//  ██████╗ ███████╗██╗      █████╗ ██████╗  █████╗
+// ██╔════╝ ██╔════╝██║     ██╔══██╗██╔══██╗██╔══██╗
+// ██║  ███╗█████╗  ██║     ███████║██║  ██║███████║
+// ██║   ██║██╔══╝  ██║     ██╔══██║██║  ██║██╔══██║
+// ╚██████╔╝███████╗███████╗██║  ██║██████╔╝██║  ██║
+//  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝
+// 
+// Copyright 2024 Sergei Bogdanov <syubogdanov@outlook.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lib/threading/tempfile/tempfile.hpp"
+
+#include <cstdio>
+#include <fstream>
+#include <mutex>
+
+static std::mutex mutex;
+
+std::filesystem::path threading::tempfile::mkdtemp(void) {
+    std::lock_guard lock(mutex);
+
+    auto dirname = std::tmpnam(nullptr);
+    std::filesystem::create_directories(dirname);
+
+    return std::filesystem::path(dirname);
+}
+
+std::filesystem::path threading::tempfile::mkstemp(void) {
+    std::lock_guard lock(mutex);
+
+    auto filename = std::tmpnam(nullptr);
+    std::ofstream{filename};
+
+    return std::filesystem::path(filename);
+}
