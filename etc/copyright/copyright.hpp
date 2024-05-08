79067611d14a05871bb3f4facbce55dafd84ed7b
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-05-08 18:08:07 +0300

    DEV - add the configuration files

---
+++ etc/copyright/copyright.hpp
@@ -0,0 +1,35 @@
+//  ██████╗ ███████╗██╗      █████╗ ██████╗  █████╗
+// ██╔════╝ ██╔════╝██║     ██╔══██╗██╔══██╗██╔══██╗
+// ██║  ███╗█████╗  ██║     ███████║██║  ██║███████║
+// ██║   ██║██╔══╝  ██║     ██╔══██║██║  ██║██╔══██║
+// ╚██████╔╝███████╗███████╗██║  ██║██████╔╝██║  ██║
+//  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝
+//
+// Copyright 2024 Sergei Bogdanov <syubogdanov@outlook.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ETC_COPYRIGHT_COPYRIGHT_HPP_
+#define ETC_COPYRIGHT_COPYRIGHT_HPP_
+
+namespace etc::copyright {
+
+constexpr const char* authors[] = {
+    "Sergei Bogdanov <syubogdanov@outlook.com>"
+};
+
+constexpr const char* license = "the Apache-2.0 license";
+
+}  // namespace etc::copyright
+
+#endif  // ETC_COPYRIGHT_COPYRIGHT_HPP_
